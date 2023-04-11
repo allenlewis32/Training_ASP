c26fad8c0628b30c3cf3b8de2452ab0c8e2b1771
Allen Lewis <allenlewis32@gmail.com>
2023-04-11 13:50:39 +0530

    CreateBook added

---
+++ WebApplication1/Pages/Books/CreateBook.cshtml.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Pages.Books
+{
+    public class CreateBookModel : PageModel
+    {
+		Book book = new();
+		public string successMessage = "", errorMessage = "";
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+			try
+			{
+				SqlConnection connection = new("Data Source=DESKTOP-IQRSRP8;initial Catalog=LMS;Integrated Security=True;Encrypt=False;");
+				connection.Open();
+
+				book.BookCode = Request.Form["bookCode"];
+				book.BookTitle = Request.Form["bookTitle"];
+				book.Category = Request.Form["category"];
+				book.Author = Request.Form["author"];
+				book.Publication = Request.Form["publication"];
+				book.PublishDate = DateOnly.FromDateTime(Convert.ToDateTime(Request.Form["publishDate"]));
+				book.BookEdition = Convert.ToInt32(Request.Form["bookEdition"]);
+				book.Price = Convert.ToInt32(Request.Form["price"]);
+				book.RackNum = Request.Form["rackNum"];
+				book.DateArrival = DateOnly.FromDateTime(Convert.ToDateTime(Request.Form["dateArrival"]));
+				book.SupplierID = Request.Form["supplierID"];
+
+				SqlCommand command = connection.CreateCommand();
+				command.CommandText = $"INSERT INTO LMS_BOOK_DETAILS VALUES('{book.BookCode}', '{book.BookTitle}', '{book.Category}', " +
+					$"'{book.Author}', '{book.Publication}', '{book.PublishDate}', {book.BookEdition}, {book.Price}, " +
+					$"'{book.RackNum}', '{book.DateArrival}', '{book.SupplierID}')";
+				command.ExecuteNonQuery();
+
+				successMessage = "Book added successfully";
+				errorMessage = "";
+				
+				connection.Close();
+			}
+			catch (Exception ex)
+			{
+				errorMessage = ex.Message;
+				successMessage = "";
+			}
+		}
+    }
+}
--- WebApplication1/Pages/Books/IndexBook.cshtml.cs
+++ WebApplication1/Pages/Books/IndexBook.cshtml.cs
@@ -4,36 +4,50 @@ using Microsoft.Data.SqlClient;
 
 namespace WebApplication1.Pages.Books
 {
-    public class IndexBookModel : PageModel
-    {
-        public List<Books> bookList = new();
-        public void OnGet()
-        {
-            SqlConnection connection = new("Data Source=DESKTOP-IQRSRP8;initial Catalog=LMS;Integrated Security=True;Encrypt=False;");
-            connection.Open();
-            SqlCommand command = connection.CreateCommand();
-            command.CommandText = "SELECT BOOK_CODE, BOOK_TITLE, AUTHOR, PUBLICATION, PRICE from LMS_BOOK_DETAILS";
-            using(var reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    Books book = new();
-                    book.Id = reader.GetString(0);
-                    book.BookName = reader.GetString(1);
-                    book.Author = reader.GetString(2);
-                    book.Publication = reader.GetString(3);
-                    book.Price = reader.GetInt32(4);
-                    bookList.Add(book);
-                }
-            }
-        }
-    }
-    public class Books
-    {
-        public string Id { get; set; }
-        public string BookName { get; set; }
-        public string Author { get; set; }
-        public string Publication { get; set; }
-        public int Price { get; set; }
-    }
+	public class IndexBookModel : PageModel
+	{
+		public List<Book> bookList = new();
+		public void OnGet()
+		{
+			try
+			{
+				SqlConnection connection = new("Data Source=DESKTOP-IQRSRP8;initial Catalog=LMS;Integrated Security=True;Encrypt=False;");
+				connection.Open();
+				SqlCommand command = connection.CreateCommand();
+				command.CommandText = "SELECT BOOK_CODE, BOOK_TITLE, AUTHOR, PUBLICATION, PRICE from LMS_BOOK_DETAILS";
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						Book book = new();
+						book.BookCode = reader.GetString(0);
+						book.BookTitle = reader.GetString(1);
+						book.Author = reader.GetString(2);
+						book.Publication = reader.GetString(3);
+						book.Price = reader.GetInt32(4);
+						bookList.Add(book);
+					}
+				}
+				connection.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+	}
+	public class Book
+	{
+		public string BookCode { get; set; }
+		public string BookTitle { get; set; }
+		public string Category { get; set; }
+		public string Author { get; set; }
+		public string Publication { get; set; }
+		public DateOnly PublishDate { get; set; }
+		public int BookEdition { get; set; }
+		public int Price { get; set; }
+		public string RackNum { get; set; }
+		public DateOnly DateArrival { get; set; }
+		public string SupplierID { get; set; }
+	}
 }
